a8cf02dbd031fcc6ad903039ba977c441142c617
Sejal <sejalbhosale5@gmail.com>
2026-09-04 02:49:06 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DOM-based-Notes-Manager/',
 })
